b43c62b2537a474f95b05d03be2699dcaa037923
Chethan J <chethantony42@gmail.com>
2023-02-05 18:50:40 +0530

    completed group chat implementation

--- app/app/asgi.py
+++ app/app/asgi.py
@@ -9,45 +9,29 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from django.urls import path
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter(
-    {
-        "http": django_asgi_app,
-    }
-)
+import chat.routing
 
+application = ProtocolTypeRouter({
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": django_asgi_app,
 
-# import os
-
-# from channels.auth import AuthMiddlewareStack
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from channels.security.websocket import AllowedHostsOriginValidator
-# from django.core.asgi import get_asgi_application
-# from django.urls import path
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-# # Initialize Django ASGI application early to ensure the AppRegistry
-# # is populated before importing code that may import ORM models.
-# django_asgi_app = get_asgi_application()
-
-# from chat.consumers import AdminChatConsumer, PublicChatConsumer
-
-# application = ProtocolTypeRouter({
-#     # Django's ASGI application to handle traditional HTTP requests
-#     "http": django_asgi_app,
-
-#     # WebSocket chat handler
-#     "websocket": AllowedHostsOriginValidator(
-#         AuthMiddlewareStack(
-#             URLRouter([
-#                 path("chat/admin/", AdminChatConsumer.as_asgi()),
-#                 path("chat/", PublicChatConsumer.as_asgi()),
-#             ])
-#         )
-#     ),
-# })
+    # WebSocket chat handler
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+            (chat.routing.websocket_urlpatterns)
+            )
+        )
+    ),
+})
