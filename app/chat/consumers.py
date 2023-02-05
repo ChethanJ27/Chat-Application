fe7f543fe60ed245cb672963a21e554c159fc6a0
Chethan J <chethantony42@gmail.com>
2023-02-05 18:53:22 +0530

    fixed bugs with group chat

--- app/chat/consumers.py
+++ app/chat/consumers.py
@@ -10,27 +10,30 @@ class ChatConsumer(WebsocketConsumer):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
 
-        async_to_sync(self.channel_layer.group_add(
+        async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,self.channel_name
-            ))
+            )
         
         self.accept()
 
     def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard(
+        async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name, self.channel_name
-        ))
+        )
         pass
 
+     # Receive message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
-        async_to_sync(self.channel_layer.group_send(
-            self.room_group_name, {"type":"chat_message","message":message}
-        ))
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name, {"type": "chat_message", "message": message}
+        )
 
-    def chat_message(self,event):
+    # Receive message from room group
+    def chat_message(self, event):
         message = event["message"]
 
         # Send message to WebSocket
