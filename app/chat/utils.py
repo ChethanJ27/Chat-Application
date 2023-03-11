b70e4033de9aec63098f3853b58ec0ff4c1d5e8d
Chethan J <chethantony42@gmail.com>
2023-03-11 19:52:46 +0530

    working group_chat

--- app/chat/utils.py
+++ app/chat/utils.py
@@ -3,5 +3,5 @@
 def createRoomId(user1_email,user2_email):
     u1_email = user1_email.replace('.', '_').replace('@', '-')
     u2_email = user2_email.replace('.', '_').replace('@', '-')
-    room_id = "-".join(sorted([user1_email, user2_email]))
+    room_id = "-".join(sorted([u1_email, u2_email]))
     return room_id
